477d6d0bd364001a86cb705fff6686a9d8948dd8
= <=>
2024-11-28 20:58:36 +0900

    add java support

--- lua/community.lua
+++ lua/community.lua
@@ -14,6 +14,33 @@ return {
   { import = "astrocommunity.pack.cmake" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.proto" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.java" },
+  {
+    "mfussenegger/nvim-jdtls",
+    opts = {
+      settings = {
+        java = {
+          configuration = {
+            runtimes = {
+              {
+                name = "JavaSE-11",
+                path = "/home/fedora/.sdkman/candidates/java/11.0.25-tem/",
+              },
+            },
+          },
+        },
+        format = {
+          enabled = true,
+          settings = { -- you can use your preferred format style
+            url = "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml",
+            profile = "GoogleStyle",
+          },
+        },
+      },
+    },
+  },
   -- color theme
   { import = "astrocommunity.colorscheme.catppuccin" },
   -- git
